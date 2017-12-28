fc41d14ad3996df68f25febe9dc02e1479e37a08
jhonyxf <jhonyxf@gmail.com>
2017-12-28 13:35:32 -0200

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ var provider = new firebase.auth.GoogleAuthProvider();
 firebase.auth().languageCode = 'pt';
 
 function signIn(){
+  alert('cliquei');
   firebase.auth().signInWithPopup(provider).then(function(result) {
   // This gives you a Google Access Token. You can use it to access the Google API.
   var token = result.credential.accessToken;
   // The signed-in user info.
   var user = result.user;
-  consolle.log(user);
+  console.log(user);
   // ...
   }).catch(function(error) {
   // Handle Errors here.
