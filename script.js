93bc1e9898a4d96087c88b685679b97134f41937
jhonyxf <jhonyxf@gmail.com>
2017-12-28 13:32:24 -0200

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,14 @@ var provider = new firebase.auth.GoogleAuthProvider();
 firebase.auth().languageCode = 'pt';
 
 function signIn(){
-  firebase.auth().getRedirectResult().then(function(result) {
-  if (result.credential) {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = result.credential.accessToken;
-    // ...
-  }
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a Google Access Token. You can use it to access the Google API.
+  var token = result.credential.accessToken;
   // The signed-in user info.
   var user = result.user;
-}).catch(function(error) {
+  consolle.log(user);
+  // ...
+  }).catch(function(error) {
   // Handle Errors here.
   var errorCode = error.code;
   var errorMessage = error.message;
@@ -19,6 +18,8 @@ function signIn(){
   // The firebase.auth.AuthCredential type that was used.
   var credential = error.credential;
   // ...
-});
+  });
+
+}
 
 }
